4ac867bc13717389b072f695ad95f104286d042e
Tom Seddon <tom@tmbp-w7>
2014-11-28 01:41:44 +0000

    Add command line options.
    Still to do: something sensible when kbswitch2 is already running.

--- kbswitch2/kbswitch2/kbswitch2.c
+++ kbswitch2/kbswitch2/kbswitch2.c
@@ -15,6 +15,16 @@ enum {NOTIFY_ID=1};
 //////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
 
+struct Options
+{
+	wchar_t *pLayoutToSet;
+	BOOL showWindow;
+};
+typedef struct Options Options;
+
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+
 struct Layout
 {
 	struct Layout *pNextLayout;
@@ -173,6 +183,58 @@ static void UpdateToolTip(void)
 //////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
 
+static void SetLayoutByHandle(HKL hkl)
+{
+	PostMessage(HWND_BROADCAST,WM_INPUTLANGCHANGEREQUEST,0,(LPARAM)hkl);
+}
+
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+
+static void SetLayoutByDisplayName(const wchar_t *pName)
+{
+	Layout *pFirstLayout,*pLayout;
+//	int nameLen=lstrlenW(pName);
+
+	if(!pName)
+		return;
+
+	//LOG("Layout to set: \"%S\"\n",pName);
+
+	pFirstLayout=CreateLayoutsList();
+
+	for(pLayout=pFirstLayout;pLayout;pLayout=pLayout->pNextLayout)
+	{
+		if(lstrcmpiW(pLayout->pDisplayName,pName)==0)
+		{
+			SetLayoutByHandle(pLayout->hkl);
+			break;
+		}
+
+// 		int i,displayNameLen=lstrlenW(pLayout->pDisplayName);
+// 
+// 		//LOG("Checking: %d: \"%S\"\n",displayNameLen,pLayout->pDisplayName);
+// 
+// 		for(i=0;i<=displayNameLen-nameLen;++i)
+// 		{
+// 			int result=CompareStringW(LOCALE_INVARIANT,NORM_IGNORECASE,pLayout->pDisplayName+i,nameLen,pName,nameLen);
+// 
+// 			if(result==CSTR_EQUAL)
+// 			{
+// 				SetLayoutByHandle(pLayout->hkl);
+// 				goto done;
+// 			}
+// 		}
+	}
+
+//done:;
+	DeleteLayoutList(pFirstLayout);
+	pFirstLayout=NULL;
+}
+
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+
 static void DoPopupMenu(void)
 {
 	int idLayoutsBegin,idLayoutsEnd,idExit,cmd;
@@ -228,8 +290,7 @@ static void DoPopupMenu(void)
 		while(idx-->0)
 			pLayout=pLayout->pNextLayout;
 
-		//SystemParametersInfo(SPI_SETDEFAULTINPUTLANG,0,&pLayout->hkl,0);
-		PostMessage(HWND_BROADCAST,WM_INPUTLANGCHANGEREQUEST,0,(LPARAM)pLayout->hkl);
+		SetLayoutByHandle(pLayout->hkl);
 	}
 
 	DeleteLayoutList(pFirstLayout);
@@ -239,10 +300,35 @@ static void DoPopupMenu(void)
 //////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
 
+static const char RIGHT_CLICK_MESSAGE[]="Right click here to get the kbswitch menu.";
+static const size_t RIGHT_CLICK_MESSAGE_LEN=sizeof RIGHT_CLICK_MESSAGE-1;
+
 static LRESULT CALLBACK WndProc(HWND hWnd,UINT uMsg,WPARAM wParam,LPARAM lParam)
 {
 	switch(uMsg)
 	{
+	case WM_PAINT:
+		{
+			RECT client;
+			PAINTSTRUCT ps;
+			HDC dc=BeginPaint(hWnd,&ps);
+
+			SaveDC(dc);
+
+			GetClientRect(hWnd,&client);
+			FillRect(dc,&client,GetStockObject(WHITE_BRUSH));
+
+			SelectObject(dc,GetStockObject(DEFAULT_GUI_FONT));
+			SetTextColor(dc,RGB(0,0,0));
+			SetTextAlign(dc,TA_CENTER|VTA_CENTER);
+			TextOutA(dc,client.right/2,client.bottom/2,RIGHT_CLICK_MESSAGE,RIGHT_CLICK_MESSAGE_LEN);
+
+			RestoreDC(dc,-1);
+
+			EndPaint(hWnd,&ps);
+		}
+		return 0;
+
 	case WM_INPUTLANGCHANGEREQUEST:
 		{
 			UpdateToolTip();
@@ -276,7 +362,7 @@ static LRESULT CALLBACK WndProc(HWND hWnd,UINT uMsg,WPARAM wParam,LPARAM lParam)
 //////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
 
-static BOOL CreateWnd(void)
+static BOOL CreateWnd(BOOL show)
 {
 	WNDCLASSEXA w;
 
@@ -296,13 +382,15 @@ static BOOL CreateWnd(void)
 	if(!RegisterClassExA(&w))
 		return 0;
 
-	g_hWnd=CreateWindowA(WND_CLASS_NAME,"kbswitch",WS_OVERLAPPEDWINDOW,CW_USEDEFAULT,CW_USEDEFAULT,CW_USEDEFAULT,CW_USEDEFAULT,0,0,GetModuleHandle(0),0);
+	g_hWnd=CreateWindowA(WND_CLASS_NAME,"kbswitch2",WS_OVERLAPPEDWINDOW,CW_USEDEFAULT,CW_USEDEFAULT,CW_USEDEFAULT,CW_USEDEFAULT,0,0,GetModuleHandle(0),0);
 	if(!g_hWnd)
 		return FALSE;
 
-#ifdef _DEBUG
-	ShowWindow(g_hWnd,SW_SHOW);
-#endif//_DEBUG
+	if(show)
+	{
+		UpdateWindow(g_hWnd);
+		ShowWindow(g_hWnd,SW_SHOW);
+	}
 
 	return TRUE;
 }
@@ -310,14 +398,16 @@ static BOOL CreateWnd(void)
 //////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
 
-static int Main(void)
+static int Main(const Options *options)
 {
-	if(!CreateWnd())
+	if(!CreateWnd(options->showWindow))
 		return 1;
 
 	NotifyCtl(NIM_ADD,L"");
 
 	UpdateToolTip();
+
+	SetLayoutByDisplayName(options->pLayoutToSet);
 
 	for(;;)
 	{
@@ -420,11 +510,46 @@ static BOOL RunHelpers(void)
 //////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
 
+static void ProcessCommandLine(Options *options)
+{
+	int numArgs,i;
+	LPWSTR *ppArgs=CommandLineToArgvW(GetCommandLineW(),&numArgs);
+	if(!ppArgs)
+		return;//???
+
+	for(i=1;i<numArgs;++i)
+	{
+		if(lstrcmpiW(ppArgs[i],L"/showwindow")==0)
+			options->showWindow=TRUE;
+		else
+		{
+			// Well... whatever it is, assume it's a keyboard layout...
+			LocalFree(options->pLayoutToSet);
+
+			options->pLayoutToSet=LocalAlloc(0,(lstrlenW(ppArgs[i])+1)*sizeof(wchar_t));
+			lstrcpyW(options->pLayoutToSet,ppArgs[i]);
+		}
+	}
+
+	LocalFree(ppArgs);
+	ppArgs=NULL;
+}
+
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+
 void Entry(void)
 {
 	UINT quitMsg=RegisterWindowMessageA(QUIT_MESSAGE_NAME);
 	HANDLE hMutex;
 	int result=1;
+	Options options={0,};
+
+	ProcessCommandLine(&options);
+
+#ifdef _DEBUG
+	options.showWindow=TRUE;
+#endif
 
 	hMutex=OpenMutexA(MUTEX_ALL_ACCESS,FALSE,MUTEX_NAME);
 	if(!hMutex)
@@ -435,7 +560,7 @@ void Entry(void)
 			result=1;
 		else
 		{
-			result=Main();
+			result=Main(&options);
 
 			ReleaseMutex(hMutex);
 			hMutex=NULL;
@@ -443,6 +568,9 @@ void Entry(void)
 	}
 
 	PostMessage(HWND_BROADCAST,quitMsg,0,0);
+
+	LocalFree(options.pLayoutToSet);
+	options.pLayoutToSet=NULL;
 
 	ExitProcess(result);
 }
